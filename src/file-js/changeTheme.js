01a245c2197a8b8e774cd5cd447119e3bd6d9c1f
Artem <lazarazuy@gmail.com>
2021-10-09 23:22:39 +0200

    function to theme

--- src/file-js/changeTheme.js
+++ src/file-js/changeTheme.js
@@ -8,21 +8,47 @@ const Theme = {
 
   changeToolbarTheme.addEventListener('change', onChangeTheme);
 
-  function onChangeTheme(e) {
-    e.preventDefault();
-      if (changeToolbarTheme.checked){
-        onThemeForBody.classList.add(Theme.DARK);
-        onThemeForBody.classList.remove(Theme.LIGHT);
-        localStorage.setItem('theme', 'Theme.DARK')
-      } else {
-        onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
-        localStorage.removeItem('theme');
-        localStorage.setItem('theme','Theme.LIGHT');
-      } 
+  // function onChangeTheme(e) {
+  //   e.preventDefault();
+  //     if (changeToolbarTheme.checked){
+  //       onThemeForBody.classList.add(Theme.DARK);
+  //       onThemeForBody.classList.remove(Theme.LIGHT);
+  //       localStorage.setItem('theme', 'Theme.DARK')
+  //     } 
+  //     else {
+  //       onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+  //       localStorage.removeItem('theme');
+  //       localStorage.setItem('theme','Theme.LIGHT');
+  //     } 
+  // }
+  // const correctStorage = localStorage.getItem('theme');
+  // if (correctStorage === Theme.DARK) {
+  //   changeToolbarTheme.checked = true;
+  //   onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+  // }
+
+
+
+  function onChangeTheme() {
+    if (changeToolbarTheme.checked) {
+      addRemoveTheme(Theme.DARK, Theme.LIGHT);
+      localStorage.setItem('theme', Theme.DARK);
+    } else {
+      addRemoveTheme(Theme.LIGHT, Theme.DARK);
+      localStorage.removeItem('theme');
+      localStorage.setItem('theme', Theme.LIGHT);
+    }
   }
-  const correctStorage = localStorage.getItem('theme');
-  if (correctStorage === Theme.DARK) {
+  
+  function addRemoveTheme(add, remove) {
+    onThemeForBody.classList.add(add);
+    onThemeForBody.classList.remove(remove);
+  }
+  
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === Theme.DARK) {
     changeToolbarTheme.checked = true;
-    onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+    addRemoveTheme(Theme.DARK, Theme.LIGHT);
   }
+  
  
