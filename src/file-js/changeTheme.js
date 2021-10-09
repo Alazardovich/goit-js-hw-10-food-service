3b8f951fdc849807fa58a0fb429ebb6c80fe4286
Artem <lazarazuy@gmail.com>
2021-10-09 22:33:23 +0200

    not very good, but not so bad

--- src/file-js/changeTheme.js
+++ src/file-js/changeTheme.js
@@ -1,18 +1,28 @@
 const onThemeForBody = document.body;
 const changeToolbarTheme = document.querySelector('#theme-switch-toggle');
-console.log(onThemeForBody);
+
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-  changeToolbarTheme.addEventListener('change', onChangeTheme);
-  function onChangeTheme() {
-      if (changeToolbarTheme.checked){
-        onThemeForBody.classList.toggle('Theme.LIGHT', 'Theme.DARK');
-      }; 
-    
-        onThemeForBody.classList.toggle('Theme.DARK', 'Theme.LIGHT'); 
-     
-    
 
+  changeToolbarTheme.addEventListener('change', onChangeTheme);
+
+  function onChangeTheme(e) {
+    e.preventDefault();
+      if (changeToolbarTheme.checked){
+        onThemeForBody.classList.add(Theme.DARK);
+        onThemeForBody.classList.remove(Theme.LIGHT);
+        localStorage.setItem('theme', 'Theme.DARK')
+      } else {
+        onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+        localStorage.removeItem('theme');
+        localStorage.setItem('theme','Theme.LIGHT');
+      } 
   }
+  const correctStorage = localStorage.getItem('theme');
+  if (correctStorage === Theme.DARK) {
+    changeToolbarTheme.checked = true;
+    onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+  }
+ 
--- src/file-js/template.js
+++ src/file-js/template.js
@@ -3,7 +3,7 @@ import template from '../template.hbs';
 import menuList from '../menu.json';
 
 const addTemplate = template(menuList);
-const menuItem = document.querySelector('body');
+const menuItem = document.querySelector('.js-menu');
 
 menuItem.insertAdjacentHTML('beforeend', addTemplate);
 
