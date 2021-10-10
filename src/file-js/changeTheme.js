24fcf9967d7246d05405b02e8a026041f28eab5a
Artem <lazarazuy@gmail.com>
2021-10-10 19:24:34 +0200

    add 3 varients worked 10th task

--- src/file-js/changeTheme.js
+++ src/file-js/changeTheme.js
@@ -8,47 +8,83 @@ const Theme = {
 
   changeToolbarTheme.addEventListener('change', onChangeTheme);
 
-  // function onChangeTheme(e) {
-  //   e.preventDefault();
-  //     if (changeToolbarTheme.checked){
-  //       onThemeForBody.classList.add(Theme.DARK);
-  //       onThemeForBody.classList.remove(Theme.LIGHT);
-  //       localStorage.setItem('theme', 'Theme.DARK')
-  //     } 
-  //     else {
-  //       onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
-  //       localStorage.removeItem('theme');
-  //       localStorage.setItem('theme','Theme.LIGHT');
-  //     } 
-  // }
-  // const correctStorage = localStorage.getItem('theme');
-  // if (correctStorage === Theme.DARK) {
-  //   changeToolbarTheme.checked = true;
-  //   onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
-  // }
-
-
-
-  function onChangeTheme() {
-    if (changeToolbarTheme.checked) {
-      addRemoveTheme(Theme.DARK, Theme.LIGHT);
-      localStorage.setItem('theme', Theme.DARK);
-    } else {
-      addRemoveTheme(Theme.LIGHT, Theme.DARK);
-      localStorage.removeItem('theme');
-      localStorage.setItem('theme', Theme.LIGHT);
-    }
+  onThemeForBody.classList.add(
+    localStorage.getItem('theme'=== null)
+  ? Theme.LIGHT : localStorage.getItem('theme')
+  );
+  if (onThemeForBody.cheked===Theme.DARK) {
+    changeToolbarTheme.cheked=true;
   }
-  
-  function addRemoveTheme(add, remove) {
-    onThemeForBody.classList.add(add);
-    onThemeForBody.classList.remove(remove);
+
+  function onChangeTheme(e) {
+      if (e.target.checked) {
+        onThemeForBody.classList.replace(Theme.LIGHT, Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK)
+        // return;
+      } 
+      else {
+        onThemeForBody.classList.replace(Theme.DARK,Theme.LIGHT);
+        // localStorage.removeItem('theme');
+        localStorage.setItem('theme',Theme.LIGHT);
+      } 
   }
-  
-  const currentTheme = localStorage.getItem('theme');
-  if (currentTheme === Theme.DARK) {
-    changeToolbarTheme.checked = true;
-    addRemoveTheme(Theme.DARK, Theme.LIGHT);
-  }
-  
+  const correctStorage = localStorage.getItem('theme');
  
+// =======================================================
+  // const Theme = {
+  //   LIGHT: 'light-theme',
+  //   DARK: 'dark-theme',
+  // };
+  // const checkBodyTheme = document.body.classList;
+  // const inputCheckBox = document.querySelector('#theme-switch-toggle');
+  // console.log(checkBodyTheme);
+  
+  
+
+  // checkBodyTheme.add(
+  //   localStorage.getItem('theme') === null
+  //     ? Theme.LIGHT
+  //     : localStorage.getItem('theme'),
+  // );
+  
+  // if (checkBodyTheme.value === Theme.DARK) {
+  //   inputCheckBox.checked = true;
+  // }
+  
+  // const checkedTheme = event => {
+  //   if (event.target.checked) {
+  //     localStorage.setItem('theme', Theme.DARK);
+  //     checkBodyTheme.replace(Theme.LIGHT, Theme.DARK);
+  
+  //     return;
+  //   }
+  
+  //   localStorage.setItem('theme', Theme.LIGHT);
+  //   checkBodyTheme.replace(Theme.DARK, Theme.LIGHT);
+  // };
+  // inputCheckBox.addEventListener('change', checkedTheme);
+
+  // ====================================================
+
+  // function onChangeTheme() {
+  //   if (changeToolbarTheme.checked) {
+  //     addRemoveTheme(Theme.DARK, Theme.LIGHT);
+  //     localStorage.setItem('theme', Theme.DARK);
+  //   } else {
+  //     addRemoveTheme(Theme.LIGHT, Theme.DARK);
+  //     localStorage.removeItem('theme');
+  //     localStorage.setItem('theme', Theme.LIGHT);
+  //   }
+  // }
+  
+  // function addRemoveTheme(add, remove) {
+  //   onThemeForBody.classList.add(add);
+  //   onThemeForBody.classList.remove(remove);
+  // }
+  
+  // const currentTheme = localStorage.getItem('theme');
+  // if (currentTheme === Theme.DARK) {
+  //   changeToolbarTheme.checked = true;
+  //   addRemoveTheme(Theme.DARK, Theme.LIGHT);
+  // }
+  
